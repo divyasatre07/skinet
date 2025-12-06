c920ccfb8cf44753eaa6b994680b591b5ea1df80
Nilesh Satre <nilesh123@gmail.com>
2025-12-06 07:08:40 -0800

    End of Section 3

---
+++ API/Controllers/ProductsControllers.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(IProductRepository repo) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string?brand,string?type, string? sort)
+    {
+        return Ok(await repo.GetProductsAsync(brand,type,sort));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await repo.GetProductByIdAsync(id);
+        if (product == null) return NotFound();
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        repo.AddProduct(product);
+
+        if (await repo.SaveChangesAsync())
+        {
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        return BadRequest("Problem Creating Product");
+    }
+
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if (product.Id != id || !ProductExists(id))
+            return BadRequest("cannot update this product");
+
+        repo.UpdateProduct(product);
+
+        if (await repo.SaveChangesAsync())
+        {
+            return NoContent();
+        }
+        return BadRequest("Problem updating the product");
+
+
+    }
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await repo.GetProductByIdAsync(id);
+
+        if (product == null) return NotFound();
+
+        repo.DeleteProduct(product);
+
+        if (await repo.SaveChangesAsync())
+        {
+            return NoContent();
+        }
+        return BadRequest("Problem deleting the product");
+    }
+
+    [HttpGet("Brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        return Ok(await repo.GetBrandAsync());
+    }
+
+	[HttpGet("Types")]
+	public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+	{
+		return Ok(await repo.GetTypesAsync());
+	}
+
+	private bool ProductExists(int id)
+    {
+        return repo.ProductExists(id);
+    }
+    
+}
+
---
+++ API/Program.cs
@@ -0,0 +1,47 @@
+using Core.Interfaces;
+using Infrastructure.Data; // Make sure you include your StoreContext
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+ catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
+
+
+app.Run();
